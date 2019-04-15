0920731af9c04bb406753703a9a5bdc97c6f627d
daodao1996 <971598745@qq.com>
2019-04-15 16:22:01 +0800

    add method judgeInputFormatSearchGrade

--- src/main/java/com/tw/Format.java
+++ src/main/java/com/tw/Format.java
@@ -1,5 +1,7 @@
 package com.tw;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Pattern;
 
 public class Format {
@@ -10,7 +12,7 @@ public class Format {
     }
 
     public Student judgeInputFormatAddStudent(){
-        String pattern = ".*;.&!.*";
+        String pattern = "(.+;.+)|(.+/..+)|(.+&.+)|(.+!.+)";
         String patternNumber = "[0-9]{1,2}.?[0-9]*";
         String[] studentInfo;
         if(!Pattern.matches(pattern,input) && input.split(",").length == 8){
@@ -29,5 +31,16 @@ public class Format {
         return new Student(studentInfo[0],studentInfo[1],studentInfo[2],studentInfo[3],
                 Float.parseFloat(studentInfo[4]),Float.parseFloat(studentInfo[5]),
                 Float.parseFloat(studentInfo[6]),Float.parseFloat(studentInfo[7]));
+    }
+
+    public List<String> judgeInputFormatSearchGrade(){
+        String patternNumber = "[0-9]+(,[0-9]+)*";
+        List<String> studentsId = new ArrayList<>();
+        if(Pattern.matches(patternNumber,input)){
+            for(String id:input.split(",")){
+                studentsId.add(id);
+            }
+        }
+        return studentsId;
     }
 }
